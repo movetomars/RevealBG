433bdbb1531a94f4198460757f7dbcab435936e3
Maia Petee <maiapetee@OS-X-Mavericks.local>
2015-04-28 20:15:24 -0600

    White Sketch

---
+++ draw.js
@@ -0,0 +1,12 @@
+/**
+ * Created by maiapetee on 4/28/15.
+ */
+
+$(document).ready(function() {
+
+    var canvas = document.getElementById("canvas");
+
+    var context = canvas.getContext("2d");
+
+})
+
